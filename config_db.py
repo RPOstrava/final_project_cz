017e45ac144edcb2b39fa663f32db76405a08103
RPOstrava <122346730+RPOstrava@users.noreply.github.com>
2023-07-20 06:56:44 +0200

    Create config_db.py
    konfigurace databaze

---
+++ config_db.py
@@ -0,0 +1,9 @@
+"""
+konfigurace databaze do ktere se budou ukladat zaznamy
+"""
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'user',
+    'password': 'password',
+    'database': 'projekt_database'
+}
